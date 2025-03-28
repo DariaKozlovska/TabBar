5f8051240119e76e7717cdca5577d3aa3b9d5fe8
DariaKozlovska <odriy.lovska@gmail.com>
2025-03-28 15:32:53 +0100

    TabBar

--- TabbarController/ContentView.swift
+++ TabbarController/ContentView.swift
@@ -9,13 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView{
+            TopRatedView()
+                .tabItem{
+                    Label("Top Rated", systemImage: "star.fill")
+                }
+            BookmarksView()
+                .tabItem{
+                    Label("Books", systemImage: "book.fill")
+                }
+            ContactsView()
+                .tabItem{
+                    Label("Contacts", systemImage: "person.crop.circle")
+                }
         }
-        .padding()
+    }
+}
+
+struct ContactsView: View {
+    var body: some View {
+        VStack {
+            Text("Contacts")
+                .font(.largeTitle)
+                .bold()
+                .padding()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .edgesIgnoringSafeArea(.all)
+        .background(Color(.darkGray))
+    }
+}
+
+struct BookmarksView: View {
+    var body: some View{
+        VStack{
+            Text("Bookmarks")
+                .font(.largeTitle)
+                .bold()
+                .padding()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .edgesIgnoringSafeArea(.all)
+        .background(Color(.gray))
+    }
+}
+
+struct TopRatedView: View{
+    var body: some View{
+        VStack{
+            Text("TopRated")
+                .font(.largeTitle)
+                .bold()
+                .padding()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .edgesIgnoringSafeArea(.all)
+        .background(Color(.lightGray))
     }
 }
 
