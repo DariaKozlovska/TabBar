4aaaa44701c5fc200df1a3dd7099ee5ad8f43c44
DariaKozlovska <odriy.lovska@gmail.com>
2025-03-28 15:12:28 +0100

    Initial Commit

---
+++ TabbarController/TabbarControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabbarControllerApp.swift
+//  TabbarController
+//
+//  Created by Daria Kozlovska on 28/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct TabbarControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
